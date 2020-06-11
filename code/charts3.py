0823fc8096035d21a8db1c8d88af6de6b033bbbe
JTrillo <trillo@uma.es>
2020-06-11 19:44:40 +0200

    Article revision. Enhanced charts

--- code/charts3.py
+++ code/charts3.py
@@ -3,14 +3,15 @@ import plotly.graph_objs as go
 
 NUM_TUBES = [500, 1000, 1500]
 FILE_SIZES = [250, 500] #KB
-FONT = dict(size=16)
+FONT = dict(size=14)
+LEGEND_FONT=dict(size=16)
 BLUE_LINE = dict(color='#1f77b4') #royalblue
 RED_LINE = dict(color='#ef553b', dash='dash')
 GREEN_LINE=dict(color='#00cc96', dash='dot')
 PURPLE_LINE=dict(color='#9467bd', dash='dashdot')
 
 def totalAcquisition(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNumTubesFixed, fiveOrgsNumTubesFixed):
-    fig = make_subplots(rows=1, cols=2, shared_yaxes=True)
+    fig = make_subplots(rows=1, cols=2, shared_yaxes=True, horizontal_spacing= 0.05)
 
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=threeOrgsFileSizeFixed, mode="lines", name="3-PN", line=BLUE_LINE), row=1, col=1)
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=fiveOrgsFileSizeFixed, mode="lines", name="5-PN", line=RED_LINE), row=1, col=1)
@@ -21,12 +22,12 @@ def totalAcquisition(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNum
     fig.add_trace(go.Scatter(x=FILE_SIZES, y=fiveOrgsNumTubesFixed, mode="lines", name="5-PN", line=RED_LINE, showlegend=False), row=1, col=2)
     fig.update_xaxes(title_text="Raw data file size (KB, 500 tubes)", row=1, col=2, range=[250, 500])
 
-    #fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000)
-    fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000, annotations=[
+    #fig.layout.update(legend=dict(y=1.15, orientation="h", font=LEGEND_FONT), width=810)
+    fig.layout.update(legend=dict(y=1.15, orientation="h", font=LEGEND_FONT), width=810, annotations=[
         dict(
-            x=0.01, y=-0.3, showarrow=False, text="a) Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
+            x=0, y=-0.3, showarrow=False, text="<b>a)</b> Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
         ), dict(
-            x=0.99, y=-0.3, showarrow=False, text="b) Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
+            x=1, y=-0.3, showarrow=False, text="<b>b)</b> Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
         )
     ])
 
@@ -36,7 +37,7 @@ def totalAcquisition(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNum
     fig.show(config=config)
 
 def avgAutoAnalysis(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNumTubesFixed, fiveOrgsNumTubesFixed):
-    fig = make_subplots(rows=1, cols=2, shared_yaxes=True)
+    fig = make_subplots(rows=1, cols=2, shared_yaxes=True, horizontal_spacing= 0.05)
     
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=threeOrgsFileSizeFixed, mode="lines", name="3-PN", line=BLUE_LINE), row=1, col=1)
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=fiveOrgsFileSizeFixed, mode="lines", name="5-PN", line=RED_LINE), row=1, col=1)
@@ -47,12 +48,12 @@ def avgAutoAnalysis(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNumT
     fig.add_trace(go.Scatter(x=FILE_SIZES, y=fiveOrgsNumTubesFixed, mode="lines", name="5-PN", line=RED_LINE, showlegend=False), row=1, col=2)
     fig.update_xaxes(title_text="Raw data file size (KB, 500 tubes)", row=1, col=2, range=[250, 500])
 
-    #fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000)
-    fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000, annotations=[
+    #fig.layout.update(legend=dict(y=1.15, orientation="h", font=LEGEND_FONT), width=810)
+    fig.layout.update(legend=dict(y=1.15, orientation="h", font=LEGEND_FONT), width=810, annotations=[
         dict(
-            x=0.01, y=-0.3, showarrow=False, text="a) Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
+            x=0, y=-0.3, showarrow=False, text="<b>a)</b> Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
         ), dict(
-            x=0.99, y=-0.3, showarrow=False, text="b) Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
+            x=1, y=-0.3, showarrow=False, text="<b>b)</b> Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
         )
     ])
 
@@ -62,7 +63,7 @@ def avgAutoAnalysis(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNumT
     fig.show(config=config)
 
 def totalAutoAnalysis(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNumTubesFixed, fiveOrgsNumTubesFixed):
-    fig = make_subplots(rows=1, cols=2, shared_yaxes=True)
+    fig = make_subplots(rows=1, cols=2, shared_yaxes=True, horizontal_spacing= 0.05)
     
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=threeOrgsFileSizeFixed, mode="lines", name="3-PN", line=BLUE_LINE), row=1, col=1)
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=fiveOrgsFileSizeFixed, mode="lines", name="5-PN", line=RED_LINE), row=1, col=1)
@@ -73,12 +74,12 @@ def totalAutoAnalysis(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNu
     fig.add_trace(go.Scatter(x=FILE_SIZES, y=fiveOrgsNumTubesFixed, mode="lines", name="5-PN", line=RED_LINE, showlegend=False), row=1, col=2)
     fig.update_xaxes(title_text="Raw data file size (KB, 500 tubes)", row=1, col=2, range=[250, 500])
 
-    #fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000)
-    fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000, annotations=[
+    #fig.layout.update(legend=dict(y=1.15, orientation="h", font=LEGEND_FONT), width=810)
+    fig.layout.update(legend=dict(y=1.15, orientation="h", font=LEGEND_FONT), width=810, annotations=[
         dict(
-            x=0.01, y=-0.3, showarrow=False, text="a) Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
+            x=0, y=-0.3, showarrow=False, text="<b>a)</b> Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
         ), dict(
-            x=0.99, y=-0.3, showarrow=False, text="b) Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
+            x=1, y=-0.3, showarrow=False, text="<b>b)</b> Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
         )
     ])
 
@@ -88,7 +89,7 @@ def totalAutoAnalysis(threeOrgsFileSizeFixed, fiveOrgsFileSizeFixed, threeOrgsNu
     fig.show(config=config)
 
 def avgAnalysis(threeOrgs10FileSizeFixed, threeOrgs20FileSizeFixed, fiveOrgs10FileSizeFixed, fiveOrgs20FileSizeFixed, threeOrgs10NumTubesFixed, threeOrgs20NumTubesFixed, fiveOrgs10NumTubesFixed, fiveOrgs20NumTubesFixed):
-    fig = make_subplots(rows=1, cols=2, shared_yaxes=True)
+    fig = make_subplots(rows=1, cols=2, shared_yaxes=True, horizontal_spacing= 0.05)
     
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=threeOrgs10FileSizeFixed, mode="lines", name="3-PN, 10 analysts per role", line=BLUE_LINE), row=1, col=1)
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=threeOrgs20FileSizeFixed, mode="lines", name="3-PN, 20 analysts per role", line=GREEN_LINE), row=1, col=1)
@@ -103,12 +104,12 @@ def avgAnalysis(threeOrgs10FileSizeFixed, threeOrgs20FileSizeFixed, fiveOrgs10Fi
     fig.add_trace(go.Scatter(x=FILE_SIZES, y=fiveOrgs20NumTubesFixed, mode="lines", name="5-PN, 20 analysts per role", line=PURPLE_LINE, showlegend=False), row=1, col=2)
     fig.update_xaxes(title_text="Raw data file size (KB, 500 tubes)", row=1, col=2, range=[250, 500])
 
-    #fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000)
-    fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000, annotations=[
+    #fig.layout.update(legend=dict(y=1.25, orientation="h", font=LEGEND_FONT), width=810)
+    fig.layout.update(legend=dict(y=1.25, orientation="h", font=LEGEND_FONT), width=810, annotations=[
         dict(
-            x=0.01, y=-0.3, showarrow=False, text="a) Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
+            x=0, y=-0.3, showarrow=False, text="<b>a)</b> Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
         ), dict(
-            x=0.99, y=-0.3, showarrow=False, text="b) Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
+            x=1, y=-0.3, showarrow=False, text="<b>b)</b> Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
         )
     ])
 
@@ -118,7 +119,7 @@ def avgAnalysis(threeOrgs10FileSizeFixed, threeOrgs20FileSizeFixed, fiveOrgs10Fi
     fig.show(config=config)
 
 def totalAnalysis(threeOrgs10FileSizeFixed, threeOrgs20FileSizeFixed, fiveOrgs10FileSizeFixed, fiveOrgs20FileSizeFixed, threeOrgs10NumTubesFixed, threeOrgs20NumTubesFixed, fiveOrgs10NumTubesFixed, fiveOrgs20NumTubesFixed):
-    fig = make_subplots(rows=1, cols=2, shared_yaxes=True)
+    fig = make_subplots(rows=1, cols=2, shared_yaxes=True, horizontal_spacing= 0.05)
     
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=threeOrgs10FileSizeFixed, mode="lines", name="3-PN, 10 analysts per role", line=BLUE_LINE), row=1, col=1)
     fig.add_trace(go.Scatter(x=NUM_TUBES, y=threeOrgs20FileSizeFixed, mode="lines", name="3-PN, 20 analysts per role", line=GREEN_LINE), row=1, col=1)
@@ -133,12 +134,12 @@ def totalAnalysis(threeOrgs10FileSizeFixed, threeOrgs20FileSizeFixed, fiveOrgs10
     fig.add_trace(go.Scatter(x=FILE_SIZES, y=fiveOrgs20NumTubesFixed, mode="lines", name="5-PN, 20 analysts per role", line=PURPLE_LINE, showlegend=False), row=1, col=2)
     fig.update_xaxes(title_text="Raw data file size (KB, 500 tubes)", row=1, col=2, range=[250, 500])
 
-    #fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000)
-    fig.layout.update(legend=dict(y=1.2, orientation="h"), width=1000, annotations=[
+    #fig.layout.update(legend=dict(y=1.25, orientation="h", font=LEGEND_FONT), width=810)
+    fig.layout.update(legend=dict(y=1.25, orientation="h", font=LEGEND_FONT), width=810, annotations=[
         dict(
-            x=0.01, y=-0.3, showarrow=False, text="a) Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
+            x=0, y=-0.3, showarrow=False, text="<b>a)</b> Varying number of tubes. File size is fixed", xref="paper", yref="paper", font=FONT
         ), dict(
-            x=0.99, y=-0.3, showarrow=False, text="b) Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
+            x=1, y=-0.3, showarrow=False, text="<b>b)</b> Varying file size. Number of tubes is fixed", xref="paper", yref="paper", font=FONT
         )
     ])
 
